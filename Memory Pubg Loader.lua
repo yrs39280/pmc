1571e4f1328a50aa9397fc777e31fc842e7d29aa
Elosharp <iutchiwa@hotmail.fr>
2018-08-22 17:14:55 +0100

    sd
    sd

---
+++ Memory Pubg Loader.lua
@@ -0,0 +1,6 @@
+gg.alert("Welcome To Memory Cracked Scripts Loader")
+function zi()
+local attempt = assert(load(gg.makeRequest('https://raw.githubusercontent.com/Skypees/pmc/master/zi.txt').content))
+return attempt()
+end
+zi()
